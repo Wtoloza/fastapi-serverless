41462cec207f4e73ba29c43223c45481278f6347
Miguel A Bareno <miguelbareno@ticsocial.com.co>
2022-09-22 10:12:06 -0500

    [BUILD] update and delete routers

--- app/users/routes/delete_user_router.py
+++ app/users/routes/delete_user_router.py
@@ -1,17 +1,24 @@
 from uuid import UUID
 
-from fastapi import Path, Depends
+from fastapi import Path, Depends,HTTPException
 
 from app.users.routes.base_user_router import user_router
-
+from app.users.models import User
 from app.users.dependencies import get_user
+
+from app.core.db import Session, get_session
 
 
 @user_router.delete(
     path="/{id}"
 )
 def delete_user(
-    id: UUID = Path(...),
-    user: bool = Depends(get_user)
-):
-    return {f"Test_delete {id}": user}
+    id: UUID,
+    session: Session = Depends(get_session)):
+    user = session.delete(User, id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail=f'User {id} not found'
+        )
+    return user
--- app/users/routes/update_user_router.py
+++ app/users/routes/update_user_router.py
@@ -1,18 +1,34 @@
 from uuid import UUID
-from fastapi import Path, Depends
+from fastapi import Path, Body, Depends,HTTPException
 
 from app.users.routes.base_user_router import user_router
-from app.users.models import UserRead
-
+from app.users.models import User,UserBase, UserCreate, UserRead
 from app.users.dependencies import get_user
+
+from app.core.db import Session, get_session
 
 
 @user_router.put(
     path="/{id}",
-    # response_model=UserRead
+    response_model=UserRead
 )
 def update_user(
-    id: UUID = Path(...),
-    user: bool = Depends(get_user)
+    id: UUID,
+    user: User = Depends(get_user),
+    data: UserCreate = Body(...),
+    session: Session = Depends(get_session)
 ):
-    return 3
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail=f'User {id} not found'
+        )
+    else:
+        data = User(**data.dict())
+        for field in  data.keys():
+            user.field = data.get(str(field))
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+        return user
